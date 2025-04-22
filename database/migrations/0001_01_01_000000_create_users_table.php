25a85806452611899564f2d7c8ac7a00aec4e81d
i37406 <waqas.ciitwah@gmail.com>
2025-04-22 12:25:13 +0500

    Implement database schema with migrations as per ERD design

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone',45)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('password')->nullable();
+            $table->string('google_id',45)->nullable();
+            $table->string('reset_token')->nullable();
+            $table->timestamp('token_expiry')->nullable();
             $table->timestamps();
         });
 
